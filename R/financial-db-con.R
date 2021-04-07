cc92055dbb247d2da80289477096cafd6d1d0af1
Pachamaltese <mvargas@dcc.uchile.cl>
2021-04-07 01:20:24 -0400

    Update financial-db-con.R

--- R/financial-db-con.R
+++ R/financial-db-con.R
@@ -8,7 +8,7 @@ financial_db_con <- function() {
   err_dbedu <- tryCatch(return(dbedu_con()), error = identity)
 
   abort(paste0(
-    "Can't connect to relational.fit.cvut.cz or db-edu.pacha.dev:\n",
+    "Can't connect to relational.fit.cvut.cz or databases.pacha.dev:\n",
     conditionMessage(err_relational), "\n",
     conditionMessage(err_dbedu)
   ))
